96b1edbf06745477e63c7e63ba09abbd326a2f46
felipe <felipe.orivas@gmail.com>
2019-09-15 11:46:47 -0300

    Proyecto en desarrollo

---
+++ src/database.js
@@ -0,0 +1,29 @@
+const mysql = require('mysql');
+const {database} = require('./keys');
+const {promisify} = require('util');
+
+const pool = mysql.createPool(database);
+pool.getConnection((err, connection)=>{
+    if(err){
+        if(err.code === 'PROTOCOL_CONNECTION_LOST'){
+            console.error('DATABASE CONNECTION WAS CLOSED');
+        }
+        if(err.code === 'ER_CONT_COUNT_ERROR'){
+            console.error('DATABASE HAS TO MANY CONNECTIONS');
+        }
+        if(err.code === 'ECONNREFUSED'){
+            console.error('DATABASE CONNECTION WAS REFUSED');
+        }
+    }
+
+    if(connection){
+        connection.release();
+        console.log('DATABASE IS CONNECTED');
+    }
+
+    return;
+});
+
+pool.query = promisify(pool.query);
+
+module.exports = pool;
