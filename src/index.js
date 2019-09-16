dd2f1d8c4522d634e7daabbcd04bb10f308e21e2
felipe <felipe.orivas@gmail.com>
2019-09-16 13:25:19 -0300

    aplicando passport

--- src/index.js
+++ src/index.js
@@ -4,9 +4,14 @@ const exphbs = require('express-handlebars');
 const path = require('path');
 const flash = require('connect-flash');
 const session = require('express-session');
+const MySqlStore = require('express-mysql-session');
+const passport = require('passport');
+
+const { database } = require('./keys');
 
 // initializations
 const app = express();
+require('./lib/passport');
 
 // settings
 app.set('port', process.env.PORT || 4000);
@@ -21,10 +26,18 @@ app.engine('.hbs', exphbs({
 app.set('view engine', '.hbs');
 
 // middleware
+app.use(session({
+    secret: 'faztmysqlnodesession',
+    resave: false,
+    saveUninitialized: false,
+    store: new MySqlStore(database)
+}));
 app.use(morgan('dev'));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
 
 // global variables
 app.use((req, res, next)=>{
@@ -46,5 +59,5 @@ app.listen(app.get('port'), ()=>{
 });
 
 
-// min 2:00:00 Video del crud
+// min 2:32:00 Video del crud
 // https://www.youtube.com/watch?v=qJ5R9WTW0_E
--- src/routes/links.js
+++ src/routes/links.js
@@ -3,10 +3,19 @@ const router = express.Router();
 
 const pool = require('../database');
 
+// Show all links registered in the database
+router.get('/', async(req, res)=>{
+    const links = await pool.query('SELECT * FROM links');
+    console.log(links);
+    res.render('links/list', { links });
+});
+
+// Add View returned by this route
 router.get('/add', (req, res)=>{
     res.render('links/add');
 });
 
+// Submit form for store the data with POST method
 router.post('/add', async(req, res)=>{
     // To Know what is received
     const { title, url, description} = req.body;
@@ -20,18 +29,7 @@ router.post('/add', async(req, res)=>{
     res.redirect('/links');
 });
 
-router.get('/', async(req, res)=>{
-    const links = await pool.query('SELECT * FROM links');
-    console.log(links);
-    res.render('links/list', { links });
-});
-
-router.get('/delete/:id', async(req, res)=>{
-    const { id } = req.params;
-    await pool.query('DELETE FROM links WHERE ID = ?', [id]);
-    res.redirect('/links');
-});
-
+// Edit link returned by this route
 router.get('/edit/:id', async(req, res)=>{
     const { id } = req.params;
     const links = await pool.query('SELECT * FROM links WHERE id = ?',  [id]);
@@ -40,6 +38,7 @@ router.get('/edit/:id', async(req, res)=>{
     res.render('links/edit', { link });
 });
 
+// Update links by their id in the database
 router.post('/edit/:id', async(req, res)=>{
     const { id } = req.params;
     const { title, description, url } = req.body;
@@ -49,6 +48,15 @@ router.post('/edit/:id', async(req, res)=>{
         url
     }
     await pool.query('UPDATE links SET ? WHERE id = ?',  [newLink, id]);
+    req.flash('success', 'Link updated successfully');
+    res.redirect('/links');
+});
+
+// Delete links by their id in the database
+router.get('/delete/:id', async(req, res)=>{
+    const { id } = req.params;
+    await pool.query('DELETE FROM links WHERE ID = ?', [id]);
+    req.flash('success', 'Link removed successfully');
     res.redirect('/links');
 });
 
